04303694262997b2efedec73b8358e342aff1330
parfait2 <parkyl0208@naver.com>
2023-10-13 14:43:15 +0900

    [Add] Memory CRUD DB

--- src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
+++ src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
@@ -1,2 +1,30 @@
-package com.example.restaurant.db;public class MemoryDbRepositoryAbstract {
+package com.example.restaurant.db;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> implements MemoryDbRepositoryIfs<T> {
+    private final List<T> db = new ArrayList<>();
+    private int index = 0;
+
+    @Override
+    public Optional<T> findById(int index) {
+        return db.stream().filter(it -> it.getIndex() == index).findFirst();
+    }
+
+    @Override
+    public T save(T entity) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int index) {
+
+    }
+
+    @Override
+    public List<T> listAll() {
+        return null;
+    }
 }
