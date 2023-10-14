c37e3e5a709300ac54ab81f6f39b8065374b0a99
parfait2 <parkyl0208@naver.com>
2023-10-14 22:47:50 +0900

    [Update] save, deleteById, listAll

--- src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
+++ src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
@@ -15,16 +15,36 @@ abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> imple
 
     @Override
     public T save(T entity) {
-        return null;
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == entity.getIndex()).findFirst();
+
+        if(optionalEntity.isEmpty()) {
+            // db에 데이터가 없는 경우
+            index++;
+            entity.setIndex(index);
+            db.add(entity);
+            return entity;
+        } else {
+            // db에 이미 데이터가 있는 경우
+            var preIndex = optionalEntity.get().getIndex();
+            entity.setIndex(preIndex); //  data update
+
+            deleteById(preIndex);
+            db.add(entity);
+            return entity;
+        }
     }
 
     @Override
     public void deleteById(int index) {
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == index).findFirst();
 
+        if(optionalEntity.isPresent()) {
+            db.remove(optionalEntity.get());
+        }
     }
 
     @Override
     public List<T> listAll() {
-        return null;
+        return db;
     }
 }
