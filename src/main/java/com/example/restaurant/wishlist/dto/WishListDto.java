901f6ecf04fa3aa88d39b45dcc3a625712ef7de9
parfait2 <parkyl0208@naver.com>
2023-10-17 23:29:37 +0900

    [Add] WishListService

---
+++ src/main/java/com/example/restaurant/wishlist/dto/WishListDto.java
@@ -0,0 +1,28 @@
+package com.example.restaurant.wishlist.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * entity와 분리한 이유
+ * : 데이터베이스의 entity가 변경되면 front-end도 변수명을 변경해야 하는 영향을 끼친다.
+ * 분리하면 중간에 변환하는 과정만 있으면 된다.
+ * */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class WishListDto {
+    private int index;
+    private String title;                   // 음식명, 장소명
+    private String category;                // 카테고리
+    private String address;                 // 주소
+    private String readAddress;             // 도로명
+    private String homePageLink;            // 홈페이지 주소
+    private String imageLink;               // 음식, 가게 이미지 주소
+    private boolean isVisit;                // 방문 여부
+    private int visitCount;                 // 방문 카운트
+    private LocalDateTime lastVisitDate;    // 마지막 방문 일자
+}
